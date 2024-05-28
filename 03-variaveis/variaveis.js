09cb7843e25b6a3498f029fa59ed4d45d17e65a1
AivanDerock <aivan.derock.dev@gmail.com>
2024-05-28 20:44:10 -0300

    Aula 04

---
+++ 03-variaveis/variaveis.js
@@ -0,0 +1,32 @@
+const aula = "Aula 04";
+var variavel = "Variaveis";
+console.log(variavel, aula)
+
+var name = 'Ivan'
+
+var IvanDadosPessoais = {
+  Nome: 'Ivan',
+  Sobrenome: 'Rocha',
+  Idade: 18,
+  Endereço: 'Rua .....',
+  Trabalho: 'Auxiliar de operações'
+}
+
+function exibirNome() {
+  var name1 = 'ivan'
+  console.log(name1)
+}
+console.log(exibirNome())
+
+let name2 = 'ivan'
+if (true) {
+  let name3 = 'ana'
+  console.log(name3)
+}
+
+console.log(name2)
+
+const name4 = "ivan"
+name5 = 'Ana'
+
+console.log(name4, name5)
