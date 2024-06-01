3b9049fc448dd62c8c36be7ce1c3449524a83d1d
AivanDerock <aivan.derock.dev@gmail.com>
2024-05-31 21:48:22 -0300

    Aula 05

---
+++ 04-operadores_e_expressoes/operadores_e_expressoes.js
@@ -0,0 +1,22 @@
+const aula = "Aula 05";
+var oee = "Operadores e Expressoes";
+console.log(oee, aula)
+
+
+var user1 = 1
+var user2 = 5
+var user3 = 10
+
+console.log(user1 + user2) // Soma
+console.log(user2 - user1) // Subtração
+console.log(user2 * user1) // Multiplicação
+console.log(user2 / user1) // Divisão
+console.log(user2 % user1) // Modulo
+
+
+console.log(user2 + user1 * user3) // Prioridade
+
+console.error("ERROR 404") // Erro
+
+var nome = "Ivan" + " " + "Rocha" // Concatenação
+console.log(nome) 
