b1ac65757a77f84f944af1885eb5e348613369c3
AivanDerock <aivan.derock.dev@gmail.com>
2024-06-10 20:45:55 -0300

    Aula 09

---
+++ 13-switch/switch.js
@@ -0,0 +1,18 @@
+let numero = prompt('Escolha um número:')
+
+switch (numero) {
+  case '1':
+    alert('Voce escolheu apenas um numero!!')
+    break
+
+  case '2':
+    alert('Voce escolheu apenas dois numeros!!')
+    break
+
+  case '3':
+    alert('Voce escolheu apenas tres numeros!!')
+    break
+
+  default:
+    alert('Voce escolheu acima de 4 numeros!!')
+}
