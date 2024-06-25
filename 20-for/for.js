2ccc0128c3832b4926e32b64c26005bf494f09ff
AivanDerock <aivan.derock.dev@gmail.com>
2024-06-24 21:59:10 -0300

    For

---
+++ 20-for/for.js
@@ -0,0 +1,19 @@
+const for1 = () => {
+  for (let i = 1; i <= 10; i += 1) {
+    console.log(i)
+  }
+}
+
+for1()
+
+console.log('-----------------')
+
+const for2 = () => {
+
+  let nome = 'Ivan Rocha'
+  for (let i = 0; i < nome.length; i += 1) {
+    console.log(nome[i])
+  }
+}
+
+for2()
