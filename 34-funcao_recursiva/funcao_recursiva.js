cd316912042e3967741e7c641e609beae43e7816
AivanDerock <aivan.derock.dev@gmail.com>
2024-07-24 19:55:31 -0300

    Funcao recursiva

---
+++ 34-funcao_recursiva/funcao_recursiva.js
@@ -0,0 +1,24 @@
+const dividir = num => {
+  console.log(num)
+  if (num % 2 === 0) {
+    dividir(num / 2)
+  } else {
+    return num
+  }
+}
+
+dividir(200)
+
+const fatorial = num => {
+  console.log(`Numero: ${num}`)
+  if (num === 0) {
+    return 1
+  } else if (num === 1) {
+    return 1
+  } else {
+    console.log(`${num} * !${num - 1}`)
+    return num * fatorial(num - 1)
+  }
+}
+
+console.log(fatorial(5))
